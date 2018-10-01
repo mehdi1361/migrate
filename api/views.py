808c4421e891cb6dfeb7497a40e0a7046e07a110
mehdi1361 <mhd.mosavi@gmail.com>
2018-10-02 00:15:51 +0330

    resolve error

--- api/views.py
+++ api/views.py
@@ -231,7 +231,7 @@ class OrderViewSet(DefaultsMixin, AuthMixin, mixins.RetrieveModelMixin, mixins.L
             return Response({"id": 200, "message": serializer.data}, status=status.HTTP_200_OK)
 
         except Exception as e:
-            return Response({"id": 400, "message": e}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"id": 400, "message": "error message order does not exists"}, status=status.HTTP_400_BAD_REQUEST)
 
         except Order.DoesNotExist as e:
             return Response({"id": 400, "message": "order Does Not exisit!!!"}, status=status.HTTP_400_BAD_REQUEST)
